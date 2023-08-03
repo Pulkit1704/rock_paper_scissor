6b6caf15a5a21f2ab70b5411b5b41b96ce322ef0
Pulkit <pgoel3379@gmail.com>
2023-08-03 21:59:27 +0530

    add basic ui elements

--- index.js
+++ index.js
@@ -1,69 +1,94 @@
 
-var rock = document.querySelector("#rock"); 
-var paper = document.querySelector("#paper"); 
-var scissor = document.querySelector("#scissor"); 
-
 // the preference model defines the hierarchy of game objects, left value is preferred over the right value. 
 const preference_model = {
-    "rock": "scissor", 
+    "rock": "scissor",
     "paper": "rock",
-    "scissor": "paper", 
+    "scissor": "paper",
 }
 
-const object_choices = ["rock", "paper", "scissor"]; 
+const object_choices = ["rock", "paper", "scissor"];
 
-function get_computer_choice(){
+var user_score = 0;
+var computer_score = 0;
 
-    var random_index = Math.floor(Math.random() * object_choices.length) 
+function get_computer_choice() {
 
-    return object_choices[random_index] 
+    var random_index = Math.floor(Math.random() * object_choices.length)
+
+    return object_choices[random_index]
 }
 
-function get_player_choice(){
+function get_player_choice(e) {
 
-    var user_choice = prompt("pick a choice from rock, paper, scissor").toLowerCase();
-    
-    if(object_choices.includes(user_choice)){
-        return user_choice; 
-    }else{
+    var user_choice = e.target.id
+
+    if (Object.keys(preference_model).includes(user_choice)) {
+        return user_choice;
+    } else {
         window.alert("please select a valid choice from: rock, paper, scissor");
-        get_player_choice(); 
+        get_player_choice();
     }
 
 }
 
-// return the string of winning or loosing from the perspective of the user. 
-function user_won(user_choice, computer_choice){
 
-    var preferred_choice = preference_model[computer_choice]; 
+function user_won(user_choice, computer_choice) {
 
-    if(user_choice === preferred_choice){
-        return true; 
-    }else{
-        return false; 
+    var preferred_choice = preference_model[computer_choice];
+
+    if (user_choice === preferred_choice) {
+        return true;
+    } else {
+        return false;
     }
+
+}
+
+function playRound(e) {
+
+    if(user_score >=5 || computer_score >=5){
+        console.log("game over"); 
+        ui_update(); 
+        return; 
+    }
+
+    var user_choice = get_player_choice(e);
+    var computer_choice = get_computer_choice();
+
+    if (user_won(user_choice, computer_choice)) {
+        user_score++;
+    } else {
+        computer_score++;
+    }
+
+    console.log(`${user_score}: ${computer_score}`);
+
+}
+
+function ui_update(){
     
-}
+    var message = document.querySelector(".winner_box > #winnermessage")
 
-//todo: add a scoring system and a type check to alert for nul entries from user.
-function main(){
 
-    var user_score = 0; 
-    var computer_score = 0; 
-    for(i = 0; i < 5; i ++){
-        var computer_choice = get_computer_choice() 
-
-        var user_choice = get_player_choice(); 
-
-        if(user_won(user_choice, computer_choice)){
-            user_score++; 
-        }else{
-            computer_score++;
-        }   
-       
-        console.log(`${user_score}, ${computer_score}`);
+    if(user_score >= 5){
+        message.textContent = "user won"; 
+    }else{
+        message.textContent = "user lost"; 
     }
 
+    return 
+}
+
+
+function main() {
+
+    var rock = document.querySelector("#rock");
+    var paper = document.querySelector("#paper");
+    var scissor = document.querySelector("#scissor");
+
+    rock.addEventListener('click', playRound);
+    paper.addEventListener('click', playRound);
+    scissor.addEventListener('click', playRound);
 
 }
 
